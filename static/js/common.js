95e5c5ffa9f260a72bd4a2699bbb97d725cf47d0
Martin Salazard <martin.der.aze@gmail.com>
2016-05-30 12:35:07 +0200

    Use pure CSS for reverse-boo effect

--- static/js/common.js
+++ static/js/common.js
@@ -1,12 +1,5 @@
 
 jQuery(function() {
-	jQuery('.reverse-boo')
-		.on('mouseenter', function() {
-			jQuery(this).animate({opacity: 1},300); 
-		})
-		.on('mouseleave', function() {
-			jQuery(this).animate({opacity: 0.3},500); 
-		});
 });
 
 
